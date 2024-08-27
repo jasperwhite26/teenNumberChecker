48aee36cf0deb9facde3a8c52dea2be92153eb74
jasper <whiteana001@warren.k12.in.us>
2024-08-27 12:50:16 -0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(hasTeen(9, 99, 19));
+        System.out.println(hasTeen(23, 15, 42));
+        System.out.println(hasTeen(22, 23, 34));
+    }
+
+    private static boolean hasTeen(int num1, int num2, int num3) {
+        if(num1 >= 13 && num1 <= 19 || num2 >= 13 && num2 <= 19 || num3 >= 13 && num3 <= 19){
+            return true;
+
+        }
+        return false;
+    }
 }
